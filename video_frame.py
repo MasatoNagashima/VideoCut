eee9d35766ff5cfe6c61f1768f6d35b6874f6f10
Masato Nagashima <yuuoumaishin@akane.waseda.jp>
2024-02-05 15:11:15 +0900

    Update

--- video_frame.py
+++ video_frame.py
@@ -3,11 +3,11 @@ import os
 
 def save_frame(video_name, _frame_num):
     video_path = 'original_video/' + video_name + '.mp4'
-    result_path = 'output_image/mid_output/image-' + video_name + str(_frame_num) + '.jpg'
+    result_path = 'output_image/mid_output/image-' + video_name + str(_frame_num) + '.png'
 
     cap = cv2.VideoCapture(video_path)
     video_frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    frame_num = int(video_frame_count // 4 * _frame_num )
+    frame_num = int(video_frame_count // 5 * (_frame_num))
 
     if not cap.isOpened():
         return
@@ -19,19 +19,17 @@ def save_frame(video_name, _frame_num):
         os.makedirs(os.path.dirname(result_path), exist_ok=True)
         cv2.imwrite(result_path, frame)
 
-def concat_img(video_name):
-    img1 = cv2.imread('output_image/mid_output/image-' + video_name + '1.jpg')
-    img2 = cv2.imread('output_image/mid_output/image-' + video_name + '2.jpg')
-    img3 = cv2.imread('output_image/mid_output/image-' + video_name + '3.jpg')
-    img4 = cv2.imread('output_image/mid_output/image-' + video_name + '4.jpg')
-
-    img5 = cv2.hconcat([img1, img2, img3, img4])
-    cv2.imwrite('output_image/final_output/image-' + video_name + '.jpg', img5)
+def concat_img(video_name, _frame_num):
+    img = cv2.imread('output_image/mid_output/image-' + video_name + str(_frame_num) + '.png')
+    img_height = img.shape[0]
+    img_width = img.shape[1]
+    new_img = img[img_height//4: img_height//5 * 3, img_width//5 *2: img_width//3 * 2]
+    cv2.imwrite('output_image/final_output/image-' + video_name + str(_frame_num) + '.png', new_img)
 
 def save_frame_all(video_name):
     for i in range(4):
         save_frame(video_name, i + 1)
-    concat_img(video_name)
+        concat_img(video_name, i + 1)
 
 if __name__ == '__main__':
-    save_frame_all('9_PBOptimization')
+    save_frame_all('19_PegInHole')
